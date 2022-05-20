9d4272b031dcf70f9d39c43cf4930274eb113d59
bekmrat <105045287+bekmrat@users.noreply.github.com>
2022-05-20 13:34:55 +0600

    Add files via upload

---
+++ src/main/java/narxoz/moldir/NarxozSecurity/NarxozSecurityApplication.java
@@ -0,0 +1,13 @@
+package narxoz.moldir.NarxozSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NarxozSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NarxozSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/narxoz/moldir/NarxozSecurity/NarxozSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package narxoz.moldir.NarxozSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NarxozSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
